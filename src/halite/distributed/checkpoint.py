c60e32145bf59afee4d6786364e729c505c84202
Kim Seonghyeon <kim.seonghyeon@outlook.com>
2025-01-15 11:52:03 +0900

    Finalize checkpoint manager

--- src/halite/distributed/checkpoint.py
+++ src/halite/distributed/checkpoint.py
@@ -1,10 +1,10 @@
 import functools
-from dataclasses import dataclass, field
 import enum
-from io import BytesIO
 import time
 from typing import Any
 import os
+import shutil
+import re
 from multiprocessing import get_context
 
 import torch
@@ -92,20 +92,26 @@ def checkpoint_worker(recv, send):
     os.environ["TORCHELASTIC_USE_AGENT_STORE"] = "False"
     torch.cuda.set_device(int(os.environ["LOCAL_RANK"]))
     dist.init_process_group()
+
     try:
         while True:
             logger.debug("Checkpoint background process is done.")
             send.put(SaveDone())
+
             logger.debug("Wait for the new state_dict.")
             obj = recv.get()
             logger.debug("Received the new state_dict.")
             if isinstance(obj, Terminate):
                 logger.info("Terminating the checkpoint background process.")
+
                 return
+
             assert isinstance(obj, tuple)
+
             begin = time.monotonic()
             state, checkpoint_id = obj
             dcp.save(state, checkpoint_id=checkpoint_id)
+
             logger.info(
                 "Finish saving the checkpoint in the background process in "
                 f"{time.monotonic() - begin:.2f} seconds."
@@ -118,10 +124,10 @@ def checkpoint_worker(recv, send):
 class CheckpointManager:
     def __init__(
         self,
-        dataloader: DataLoader,
         model_parts: list[nn.Module],
         optimizers: list[torch.optim.Optimizer],
         lr_schedulers: list[torch.optim.lr_scheduler.LRScheduler],
+        dataloader: DataLoader,
         states: dict[str, Any],
         directory: str = "checkpoint",
         interval_type: str = "steps",
@@ -220,3 +226,220 @@ class CheckpointManager:
 
         dcp.save(self.states, checkpoint_id=self._create_checkpoint_id(cur_step))
         self.reset()
+
+    def _should_save(self, current_step: int, force: bool = False):
+        if not self.enable_checkpoint:
+            return False
+
+        if not force:
+            if (
+                self.interval_type == IntervalType.STEPS
+                and not current_step % self.interval == 0
+            ):
+                return False
+
+            if self.interval_type == IntervalType.SECONDS:
+                time_sync_result = (time.monotonic() - self.begin_time) >= self.interval
+                self.time_sync_result = torch.tensor(int(time_sync_result))
+
+                if self.time_sync_work is None:
+                    self.time_sync_work = dist.all_reduce(
+                        self.time_sync_result, group=self.pg, async_op=True
+                    )
+
+                    return False
+
+                elif current_step % 5 == 4:
+                    self.time_sync_work.wait()
+                    self.time_sync_work = None
+                    time_sync_result = self.time_sync_result.item()
+                    self.time_sync_result = None
+
+                    if time_sync_result == 0:
+                        return False
+
+                else:
+                    return False
+
+        if self.time_sync_work:
+            self.time_sync_work.wait()
+            self.time_sync_work = None
+            self.time_sync_result = None
+
+        return True
+
+    def _async_wait(self):
+        if self.async_mode == AsyncMode.ASYNC_WITH_PINNED_MEM:
+            logger.debug(
+                f"Waiting for the checkpoint background process to finish, {time.monotonic()=}.:.2f"
+            )
+
+            if not self.mp.is_alive():
+                raise RuntimeError("Checkpoint background process is not alive.")
+
+            _ = self.mp_queue_recv.get()
+
+        elif self.async_mode == AsyncMode.ASYNC:
+            if self.async_future is not None:
+                self.async_future.result()
+
+    def _async_with_pinned_memory(self, checkpoint_id: str):
+        try:
+            from torch.distributed._state_dict_utils import (
+                _copy_state_dict,
+                _create_cpu_state_dict,
+            )
+
+        except ImportError as e:
+            raise ImportError(
+                "Please install the latest pytorch to use async checkpointing with pinned memory."
+            )
+
+        state_dict = dcp.state_dict_saver._stateful_to_state_dict(self.states)
+
+        if self.cpu_offload_state_dict is None:
+            logger.debug(f"Preparing the CPU memory, {time.monotonic()=}.:.2f")
+            self.cpu_offload_state_dict = _create_cpu_state_dict(
+                state_dict, pin_memory=True, share_memory=True
+            )
+
+        logger.debug(f"Staging the state_dict, {time.monotonic()=}.:.2f")
+        with torch.cuda.stream(self.staging_stream):
+            self.cpu_offload_state_dict = _copy_state_dict(
+                state_dict, self.cpu_offload_state_dict, non_blocking=True
+            )
+            self.staging = True
+            self.staging_id = checkpoint_id
+
+    def save(self, current_step: int, force: bool):
+        if not self._should_save(current_step, force):
+            return
+
+        begin = time.monotonic()
+        checkpoint_id = self._create_checkpoint_id(current_step)
+        self._async_wait()
+
+        if force:
+            self._save_last_step(current_step)
+
+        elif self.async_mode == AsyncMode.ASYNC_WITH_PINNED_MEM:
+            self._async_with_pinned_memory(checkpoint_id)
+
+        elif self.async_mode == AsyncMode.ASYNC:
+            self.async_future = dcp.async_save(
+                self.states, checkpoint_id=checkpoint_id, process_group=self.pg
+            )
+
+        else:
+            dcp.save(self.states, checkpoint_id=checkpoint_id)
+
+        self.reset()
+        self._purge_stale_checkpoints()
+
+        logger.info(
+            "Finished saving the checkpoint (or staging if async is enabled) in "
+            f"{time.monotonic() - begin:.2f} seconds."
+        )
+
+    def maybe_wait_for_staging(self):
+        if (
+            self.enable_checkpoint
+            and self.async_mode == AsyncMode.ASYNC_WITH_PINNED_MEM
+            and self.staging
+        ):
+            if not self.staging_stream.query():
+                self.staging_stream.synchronize()
+
+            def sync_func():
+                self.mp_queue_send.put_nowait(
+                    (self.cpu_offload_state_dict, self.staging_id)
+                )
+
+            sync_func()
+            self.staging = False
+
+    def load(self, step: int = -1):
+        if not self.enable_checkpoint:
+            return False
+
+        if not os.path.isdir(self.directory):
+            return False
+
+        if step != -1 and not os.path.isdir(self._create_checkpoint_id(step)):
+            return False
+
+        if step == -1:
+            step_counts = []
+
+            for filename in os.listdir(self.directory):
+                match = re.search(r"step-(\d+)", filename)
+                metadata_probe = os.path.join(self.directory, filename, ".metadata")
+
+                if match and os.path.isfile(metadata_probe):
+                    step_counts.append(int(match.group(1)))
+
+            if not step_counts:
+                return False
+
+            step = max(step_counts)
+
+        states = {"model": self.states["model"]} if step == 0 else self.states
+        original_stateful_states = {
+            k: v for k, v in states.items() if isinstance(v, Stateful)
+        }
+        logger.info(f"Loading the checkpoint at step {step}")
+        begin = time.monotonic()
+        dcp.load(states, checkpoint_id=self._create_checkpoint_id(step))
+        logger.info(
+            f"Finished loading the checkpoint in {time.monotonic() - begin:.2f} seconds"
+        )
+
+        states.update(original_stateful_states)
+
+        return True
+
+    def _purge_stale_checkpoints(self):
+        if self.keep_latest_k <= 0:
+            return
+
+        discovered_checkpoints = []
+        for filename in os.listdir(self.directory):
+            match = re.search(r"step-(\d+)", filename)
+            path = os.path.join(self.directory, filename)
+            discovered_checkpoints.append((int(match.group(1)), path))
+
+        discovered_checkpoints.sort()
+        to_delete = discovered_checkpoints[: -1 * self.keep_latest_k]
+
+        for _, path in to_delete:
+            logger.info(f"Deleting old checkpoint {path}")
+            shutil.rmtree(path, ignore_errors=True)
+
+
+def load_checkpoint(
+    path: str, model_parts=None, optimizers=None, states=None, verbose: bool = False
+):
+    if states is None:
+        states = {}
+
+    if model_parts is not None:
+        states["model"] = ModelWrapper(model_parts)
+
+    if optimizers is not None:
+        states["optimizer"] = OptimizerWrapper(model_parts, optimizers)
+
+    original_stateful_states = {
+        k: v for k, v in states.items() if isinstance(v, Stateful)
+    }
+
+    if verbose:
+        logger.info(f"Loading the checkpoint of {path}")
+        begin = time.monotonic()
+
+    dcp.load(states, checkpoint_id=path)
+    states.update(original_stateful_states)
+
+    if verbose:
+        logger.info(
+            f"Finished loading the checkpoint in {time.monotonic() - begin:.2f} seconds"
+        )
