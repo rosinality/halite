fbabbf9eff77352184bcb896607af0187dfa96f4
Kim Seonghyeon <kim.seonghyeon@outlook.com>
2025-01-24 14:30:17 +0900

    Finalize checkpoint manager

--- src/halite/distributed/__init__.py
+++ src/halite/distributed/__init__.py
@@ -1,6 +1,14 @@
 import torch
 from torch import distributed as dist
 
+from halite.distributed.checkpoint import CheckpointManager, load_checkpoint
+from halite.distributed.managers import (
+    ModelManager,
+    OptimizerManager,
+    LRSchedulerManager,
+)
+
+
 def all_reduce_mean(tensor, group, world_size):
     dist.all_reduce(tensor, group=group)
     tensor = tensor / world_size
@@ -10,7 +18,7 @@ def all_reduce_mean(tensor, group, world_size):
 
 def all_reduce_flag(flag, group, device):
     flag_tensor = torch.tensor(int(flag), dtype=torch.float32, device=device)
-    
+
     dist.all_reduce(flag_tensor, group=group, op=dist.ReduceOp.MAX)
     flag = flag_tensor.item() > 0
 
