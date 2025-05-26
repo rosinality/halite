763e3fb466d81d8d4f5a062bc93019f126d3f72a
Kim Seonghyeon <kim.seonghyeon@outlook.com>
2025-05-26 09:15:47 +0900

    Better support for tokenizer checkpointing

--- src/halite/projects/common/config.py
+++ src/halite/projects/common/config.py
@@ -11,12 +11,50 @@ from slickconf import Config, Instance, exempt, deserialize, field
 from halite.transformers import TransformerConfig
 
 
+def check_tokenizer_exists(checkpoint_path: str):
+    tokenizer_path = os.path.join(checkpoint_path, "tokenizer.model")
+    if os.path.exists(tokenizer_path):
+        return tokenizer_path
+
+    tokenizer_path = os.path.join(checkpoint_path, "tokenizer.json")
+    if os.path.exists(tokenizer_path):
+        return checkpoint_path
+
+    return None
+
+
+def get_tokenizer_files(checkpoint_path: str):
+    tokenizer_path = os.path.join(checkpoint_path, "tokenizer.model")
+    if os.path.exists(tokenizer_path):
+        return [tokenizer_path]
+
+    tokenizer_paths = []
+    tokenizer_path = os.path.join(checkpoint_path, "tokenizer.json")
+    if os.path.exists(tokenizer_path):
+        tokenizer_paths.append(tokenizer_path)
+
+    tokenizer_path = os.path.join(checkpoint_path, "tokenizer_config.json")
+    if os.path.exists(tokenizer_path):
+        tokenizer_paths.append(tokenizer_path)
+
+    tokenizer_path = os.path.join(checkpoint_path, "vocab.json")
+    if os.path.exists(tokenizer_path):
+        tokenizer_paths.append(tokenizer_path)
+
+    tokenizer_path = os.path.join(checkpoint_path, "merges.txt")
+    if os.path.exists(tokenizer_path):
+        tokenizer_paths.append(tokenizer_path)
+
+    return tokenizer_paths
+
+
 class Model(Config):
     model_conf: TransformerConfig | None = None
     model: Instance | None = None
     model_infer: Instance | None = None
     tokenizer: Instance | None = None
     parallelize: Instance | None = None
+    parallelize_infer: Instance | None = None
     wrapper: Instance | None = None
     policy: dict[str, Any] | None = None
     checkpoint_path: StrictStr | None = None
@@ -43,8 +81,11 @@ class Model(Config):
         if "parallelize" in conf:
             self.parallelize = Instance(conf["parallelize"])
 
-        tokenizer_path = os.path.join(self.checkpoint_path, "tokenizer.model")
-        if os.path.exists(tokenizer_path):
+        if "parallelize_infer" in conf:
+            self.parallelize_infer = Instance(conf["parallelize_infer"])
+
+        tokenizer_path = check_tokenizer_exists(self.checkpoint_path)
+        if tokenizer_path is not None:
             self.tokenizer.model_path = tokenizer_path
 
         if "model_conf" in conf:
@@ -72,8 +113,11 @@ def load_model(checkpoint_path):
     if "parallelize" in model_conf:
         conf.parallelize = model_conf["parallelize"]
 
-    tokenizer_path = os.path.join(checkpoint_path, "tokenizer.model")
-    if os.path.exists(tokenizer_path):
+    if "parallelize_infer" in model_conf:
+        conf.parallelize_infer = model_conf["parallelize_infer"]
+
+    tokenizer_path = check_tokenizer_exists(checkpoint_path)
+    if tokenizer_path is not None:
         conf.tokenizer.model_path = tokenizer_path
 
     if "model_conf" in model_conf:
@@ -105,6 +149,7 @@ def save_model(model_config, checkpoint_path):
     conf["model_infer"] = ensure_dict(model_config.model_infer)
     conf["tokenizer"] = ensure_dict(model_config.tokenizer)
     conf["parallelize"] = ensure_dict(model_config.parallelize)
+    conf["parallelize_infer"] = ensure_dict(model_config.parallelize_infer)
     conf["model_conf"] = ensure_dict(model_config.model_conf)
 
     with open(os.path.join(checkpoint_path, "config.json"), "w") as f:
@@ -113,17 +158,19 @@ def save_model(model_config, checkpoint_path):
     if model_config.tokenizer is None:
         return
 
-    tokenizer_path = model_config.tokenizer.model_path
-
-    if os.path.exists(tokenizer_path):
-        shutil.copy(tokenizer_path, os.path.join(checkpoint_path, "tokenizer.model"))
+    tokenizer_files = get_tokenizer_files(model_config.tokenizer.model_path)
+    for tokenizer_file in tokenizer_files:
+        shutil.copy(
+            tokenizer_file,
+            os.path.join(checkpoint_path, os.path.basename(tokenizer_file)),
+        )
 
     conf = model_config.tokenizer
 
     if not isinstance(conf, dict):
         conf = conf.to_dict()
 
-    with open(os.path.join(checkpoint_path, "tokenizer_config.json"), "w") as f:
+    with open(os.path.join(checkpoint_path, "halite_tokenizer_config.json"), "w") as f:
         json.dump(conf, f, indent=2)
 
 
@@ -132,9 +179,7 @@ def get_tokenizer(model):
         tokenizer = copy.deepcopy(model["tokenizer"])
 
         if "checkpoint_path" in model:
-            tokenizer["model_path"] = os.path.join(
-                model["checkpoint_path"], "tokenizer.model"
-            )
+            tokenizer["model_path"] = check_tokenizer_exists(model["checkpoint_path"])
 
         return Instance(tokenizer)
 
@@ -142,14 +187,12 @@ def get_tokenizer(model):
         raise ValueError("tokenizer or checkpoint is not specified")
 
     with open(
-        os.path.join(model["checkpoint_path"], "tokenizer_config.json"), "r"
+        os.path.join(model["checkpoint_path"], "halite_tokenizer_config.json"), "r"
     ) as f:
         conf = json.load(f)
 
     tokenizer = Instance(conf)
-    tokenizer_path = os.path.join(model["checkpoint_path"], "tokenizer.model")
-    if os.path.exists(tokenizer_path):
-        tokenizer.model_path = tokenizer_path
+    tokenizer.model_path = check_tokenizer_exists(model["checkpoint_path"])
 
     return tokenizer
 
