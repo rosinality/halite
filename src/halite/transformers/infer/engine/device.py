c23bbccd8c4515b7aafe0a75fa859d37f1858209
Kim Seonghyeon <kim.seonghyeon@outlook.com>
2025-05-28 09:41:53 +0900

    Revised available memory calculation logit for inference engine

--- src/halite/transformers/infer/engine/device.py
+++ src/halite/transformers/infer/engine/device.py
@@ -1,11 +1,11 @@
 import torch
 
 
-def get_available_gpu_memory(device, gpu_id, distributed=False):
+def get_gpu_memory(device, gpu_id, distributed=False):
     if device == "cuda":
         n_gpus = torch.cuda.device_count()
         torch.cuda.empty_cache()
-        free_memory, _ = torch.cuda.mem_get_info(gpu_id)
+        free_memory, total_memory = torch.cuda.mem_get_info(gpu_id)
 
     if distributed:
         buffer = torch.tensor(free_memory, dtype=torch.float32).to(
@@ -14,4 +14,10 @@ def get_available_gpu_memory(device, gpu_id, distributed=False):
         torch.distributed.all_reduce(buffer, op=torch.distributed.ReduceOp.MIN)
         free_memory = buffer.item()
 
-    return free_memory / (1 << 30)
+        buffer = torch.tensor(total_memory, dtype=torch.float32).to(
+            torch.device(device, gpu_id)
+        )
+        torch.distributed.all_reduce(buffer, op=torch.distributed.ReduceOp.MIN)
+        total_memory = buffer.item()
+
+    return free_memory / (1 << 30), total_memory / (1 << 30)
