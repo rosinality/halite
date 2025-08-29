4a70b02ce5ecb6a50171e41124d14729c147b9dd
Kim Seonghyeon <kim.seonghyeon@outlook.com>
2025-08-29 03:00:23 +0000

    Clean up unused codes

--- src/halite/transformers/srt/model_runner.py
+++
@@ -1,9 +0,0 @@
-from sglang.srt.model_executor import model_runner
-
-
-class ModelRunner(model_runner.ModelRunner):
-    def init_torch_distributed(self):
-        pass
-
-    def load_model(self):
-        pass
